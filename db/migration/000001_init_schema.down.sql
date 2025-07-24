1090131b733f38f461f2dc8fc66a191befb688e9
Luan Kraisch <52788268+luan-k@users.noreply.github.com>
2025-07-24 14:50:26 +0200

    up and down migration

---
+++ db/migration/000001_init_schema.down.sql
@@ -0,0 +1,17 @@
+DROP INDEX IF EXISTS "unique_post_image";
+
+DROP INDEX IF EXISTS "unique_post_category";
+
+DROP TABLE IF EXISTS "post_images";
+
+DROP TABLE IF EXISTS "images";
+
+DROP TABLE IF EXISTS "sessions";
+
+DROP TABLE IF EXISTS "posts_categories";
+
+DROP TABLE IF EXISTS "categories";
+
+DROP TABLE IF EXISTS "posts";
+
+DROP TABLE IF EXISTS "users";
---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,76 @@
+CREATE TABLE "users" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "username" varchar UNIQUE NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "hashed_password" varchar NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "role" varchar NOT NULL DEFAULT 'User'
+);
+
+CREATE TABLE "posts" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "description" varchar NOT NULL,
+  "user_id" bigint NOT NULL,
+  "username" varchar NOT NULL DEFAULT '',
+  "images" varchar[] NOT NULL DEFAULT '{}',
+  "url" varchar UNIQUE NOT NULL DEFAULT '',
+  "created_at" timestamptz NOT NULL DEFAULT (now()::timestamp;),
+  "changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z'
+);
+
+CREATE TABLE "categories" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "description" varchar NOT NULL
+);
+
+CREATE TABLE "posts_categories" (
+  "post_id" bigint NOT NULL,
+  "category_id" bigint NOT NULL
+);
+
+CREATE TABLE "sessions" (
+  "id" uuid PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "refresh_token" varchar NOT NULL,
+  "user_agent" varchar NOT NULL,
+  "client_ip" varchar NOT NULL,
+  "is_blocked" boolean NOT NULL DEFAULT false,
+  "expires_at" timestamptz NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "images" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "description" varchar NOT NULL,
+  "alt" varchar NOT NULL,
+  "image_path" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z'
+);
+
+CREATE TABLE "post_images" (
+  "post_id" bigint NOT NULL,
+  "image_id" bigint NOT NULL,
+  "order" int NOT NULL DEFAULT 0
+);
+
+CREATE UNIQUE INDEX "unique_post_category" ON "posts_categories" ("post_id", "category_id");
+
+CREATE UNIQUE INDEX "unique_post_image" ON "post_images" ("post_id", "image_id");
+
+ALTER TABLE "posts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "posts_categories" ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
+
+ALTER TABLE "posts_categories" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
+
+ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
+
+ALTER TABLE "post_images" ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
+
+ALTER TABLE "post_images" ADD FOREIGN KEY ("image_id") REFERENCES "images" ("id");
